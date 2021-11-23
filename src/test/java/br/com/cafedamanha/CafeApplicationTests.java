e5ae65dfcf5c55c3749c5131f6e4808d6f3ceee9
Bruno Luna <luna.brsilva@gmail.com>
2021-11-23 08:34:54 -0300

    CafeAplicationTests

---
+++ src/test/java/br/com/cafedamanha/CafeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cafedamanha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
